212ab4a80a9f8a607db3ffc6a2fd4693f2fa54b4
NicolasD2 <nicolas.diaz03@alumnos.ucn.cl>
2024-10-02 20:01:32 -0300

    feat:Creacion de las clases

---
+++ src/Data/DataSeeder.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiPrueba.src.Data
+{
+    public class DataSeeder
+    {
+        
+    }
+}
